dd8f71f20544c0a7253170875b75ce5b4122ea75
BlairGW <blairgolds@outlook.com>
2024-04-11 10:32:55 +0100

    Internal Commentation and security thing

--- main.js
+++ main.js
@@ -2,7 +2,18 @@ function formSubmit(){
     document.getElementById("form_outcome").innerHTML="Form has been sent Successfully!"
 }
 
-counter = 2;
+function login() {
+  var title = document.forms["email"]["email"].value;
+  var date = document.forms["password"]["password"].value;
+  if (title == "") {
+  alert("You must enter a valid email address.");
+  }
+  else {
+      if (date.length == 0) {
+      alert("Please enter your valid password.");
+      }
+      else {
+        counter = 2;
         function login(){
           if(counter % 2 == 0){
             alert("You Have been logged in!")
@@ -11,3 +22,7 @@ counter = 2;
           }
           counter += 1;
         }
+      }
+  }
+}
+
